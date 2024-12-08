a2abffdf6190fcb30bf63a3672b07fa7d0e04240
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-12-08 13:51:53 +1100

    added unique property for email + important comments

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -3,14 +3,15 @@ export interface IUser {
     name: string;
     email: string;
     password: string;
-    tasks: mongoose.Types.ObjectId[];
+    tasks: mongoose.Types.ObjectId[]; // array of object ID's referencing Task collection
 }
 
 const userSchema = new mongoose.Schema<IUser>({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    tasks: [{type: mongoose.Types.ObjectId, ref: "Task"}]
+    // ref property below defines a relationship between user and task collection in mongodb
+    tasks: [{type: mongoose.Types.ObjectId, ref: "Task"}] 
 })
 
 export default mongoose.model<IUser>("Users", userSchema);
