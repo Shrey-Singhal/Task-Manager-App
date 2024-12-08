5262a31c02df1ba7150a52e75148b7a5ee1115a2
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-12-08 16:54:26 +1100

    add task for a specific user method

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,3 +1,4 @@
+import Task from "../models/task";
 import User from "../models/user";
 import { Response, Request, NextFunction } from "express";
 
@@ -7,6 +8,34 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
         const newUser = new User({name, email, password});
         await newUser.save();
         res.status(201).json({message: `New User: ${name} created successfully`, user: newUser});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const addUserTask = async (req: Request, res: Response, next: NextFunction) => {
+    const {userId} = req.params;
+    const { title, description, status, dueDate } = req.body;
+    
+    try {
+        const user = await User.findById(userId);
+        const newTask = new Task({
+            title,
+            description,
+            status,
+            dueDate
+        });
+        await newTask.save();
+
+        user?.tasks?.push(newTask._id);
+        await user?.save();
+
+        res.status(201).json({
+            message: "Task created and added to user successfully",
+            task: newTask,
+            user,
+        });
+        
     } catch (error) {
         next(error);
     }
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,5 +3,6 @@ import * as UserController from "../controllers/userController"
 const router = Router();
 
 router.post("/", UserController.createUser);
+router.post("/:userId/tasks", UserController.addUserTask);
 
 export default router;
