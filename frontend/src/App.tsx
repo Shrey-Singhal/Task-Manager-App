0ed2b371f98eb200a6f4a92863d1b15a47b97315
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-12-10 16:09:53 +1100

    remove unused pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { SidebarProvider } from "./contexts/sidebarContext";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
-import ToDo from "./pages/ToDo";
-import InProgress from "./pages/InProgress";
-import Completed from "./pages/Completed";
 import Trash from "./pages/Trash";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -31,9 +28,6 @@ function App() {
             }>
             <Route index path='/' element={<Navigate to='/login' />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/todo" element={<ToDo />} />
-            <Route path="/inprogress" element={<InProgress />} />
-            <Route path="/completed" element={<Completed />} />
             <Route path="/trash" element={<Trash />} />
           </Route>
           <Route path="/login" element={<Login />} />
--- frontend/src/components/APPNAV_ITEMS.tsx
+++ frontend/src/components/APPNAV_ITEMS.tsx
@@ -1,25 +1,10 @@
-import { Home, ListTodo, CircleCheckBig, Trash2 } from "lucide-react";
+import { Home,Trash2 } from "lucide-react";
 
 export const APPNAV_ITEMS = [
   {
     title: "Dashboard",
     path: "/dashboard",
     icon: <Home />,
-  },
-  {
-    title: "To Do",
-    path: "/todo",
-    icon: <ListTodo />,
-  },
-  {
-    title: "In Progress",
-    path: "/inprogress",
-    icon: <ListTodo />
-  },
-  {
-    title: "Completed",
-    path: "/completed",
-    icon: <CircleCheckBig />
   },
   {
     title: "Trash",
