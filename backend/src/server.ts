2aee6693fa58d37c4f071762bfc5e2c9e42db0d8
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-12-10 21:22:37 +1100

    fixed port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 
 import mongoose from "mongoose";
 import app from "./app"
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
     .then(() => {
